c48f29ab371723d586f63774c9b6014ed5fa84ba
Anupama <jasinfoss@gmail.com>
2022-02-15 20:58:34 -0800

    Connection of database postgresql with django has been done through the connector psycopg2 and changed the text contents of home page.

--- cakehutt/cakehutt/settings.py
+++ cakehutt/cakehutt/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'cakehutt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cakehuttdb',
+        'USER' : 'postgres',
+        'PASSWORD': 'anu@0598',
+        'HOST': 'localhost'
+            }
 }
 
 
