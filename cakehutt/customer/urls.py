f7155ff093b18cf58cc00ede3b76d568c9cb32ae
Anupama <jasinfoss@gmail.com>
2022-02-21 21:48:16 -0800

    Superuser is created for admin.Other users are added using POST method through register page and a succesfully registered message is returned.For checking whether an user is already exit User.objects.filter() is used.If the username and email is exists which return an error message.When both the password and re-entered password is not matching which return an error message as password is not matching. auth.athenticate() is used for authentication purpose of username and password.If the authenticated user is not none then the user request for login through auth.login() and redirect to the home page.Csrf_token feature is added for providing an extra protection by generating unique codes.

--- cakehutt/customer/urls.py
+++ cakehutt/customer/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/',views.index,name='loginpage'),
+    path('login/',views.login,name='loginpage'),
     path('register/',views.register,name='registerpage'),
+    path('register/register2/',views.register2,name='register2page'),
+    path('login/login1',views.login1,name='login1page')
+    
 ]
--- cakehutt/customer/views.py
+++ cakehutt/customer/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
 
-def index(request):
+def login(request):
     return render(request,'login.html')
 
 def register(request):
     return render(request,'register.html')
+
+def register2(request):
+    uname = request.POST['Uname']
+    fname = request.POST['Fname']
+    lname = request.POST['Lname']
+    ename = request.POST['Ename']
+    pname = request.POST['Pname']
+    repname = request.POST['Repname']
+    if pname == repname:
+        if User.objects.filter(username = uname).exists():
+            umsg="Username is taken!"
+            return render(request,'register.html',{'Umsg':umsg})
+        elif User.objects.filter(email = ename).exists():
+            emsg="Email is taken!"
+            return render(request,'register.html',{'Emsg':emsg})
+        else:
+            user=User.objects.create_user(username=uname,first_name=fname,last_name=lname,email=ename,password=pname)
+            user.save();
+            return render(request,'success.html')
+
+    else:
+        pmsg="Password is not matching!"
+        return render(request,'register.html',{'Pmsg':pmsg})
+
+def login1(request):
+    uname = request.POST['Uname']
+    pname = request.POST['Pname']
+    user = auth.authenticate(username=uname,password=pname)
+    if user is not None:
+        auth.login(request,user)
+        return redirect('/')
+    else:
+        return render(request,'login.html')
