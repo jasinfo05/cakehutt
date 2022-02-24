83913f31207aaaa66c0b60aa0f69996896c3f199
Anupama <jasinfoss@gmail.com>
2022-02-24 15:33:11 -0800

    The cake model was created but not made any migrations

--- cakehutt/home/models.py
+++ cakehutt/home/models.py
@@ -1,8 +1,14 @@
+from pickle import FALSE, TRUE
 from tkinter import Image
 from tokenize import Name
 from django.db import models
 
-class cake:
-    Name : str
-    Image : str
-    Price : float
+class cake(models.Model):
+    Name = models.CharField(max_length=100)
+    Image = models.ImageField(upload_to='pics')
+    Price = models.IntegerField()
+    Stock = models.FloatField()
+    Date = models.DateTimeField(auto_now_add=True)
+    Desc=models.TextField()
+    Offer = models.FloatField()
+    Offer2 = models.BooleanField(default=False)
