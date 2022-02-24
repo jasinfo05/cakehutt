02615cbb54aeba047d1832e5625f5dad90d45518
Anupama <jasinfoss@gmail.com>
2022-02-24 15:05:23 -0800

    A class is created inside models.py in home.Three values are given as Name,Price,Image in the class.Objects created for class inside views.Then the objects sends to index page. A list is created for objects and the list given in a loop.

--- cakehutt/cakehutt/urls.py
+++ cakehutt/cakehutt/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('customer/',include('customer.urls'))
+    path('customer/',include('customer.urls')),
+    path('product/',include('product.urls'))
 ]
---
+++ cakehutt/home/models.py
@@ -0,0 +1,8 @@
+from tkinter import Image
+from tokenize import Name
+from django.db import models
+
+class cake:
+    Name : str
+    Image : str
+    Price : float
